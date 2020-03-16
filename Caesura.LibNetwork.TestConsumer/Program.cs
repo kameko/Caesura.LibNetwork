6c1873e1540109459c59724d605abf34bcfe6bea
Kameko Kirishiki <kameko.k@outlook.com>
2020-03-16 06:52:04 -0400

    test consumer project for unit testing

---
+++ Caesura.LibNetwork.TestConsumer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Caesura.LibNetwork.TestConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
