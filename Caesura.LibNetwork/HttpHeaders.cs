65664dd8c84feb61aa4c109fdb8a624218867ddb
Kameko Kirishiki <kameko.k@outlook.com>
2020-04-05 16:28:47 -0400

    implement IEnumerable

--- Caesura.LibNetwork/HttpHeaders.cs
+++ Caesura.LibNetwork/HttpHeaders.cs
@@ -2,17 +2,34 @@
 namespace Caesura.LibNetwork
 {
     using System;
+    using System.Collections;
     using System.Collections.Generic;
     using System.Linq;
     using System.Threading.Tasks;
     
-    public class HttpHeaders
+    public class HttpHeaders : IEnumerable<HttpHeader>
     {
         private List<HttpHeader> Headers;
         
         public HttpHeaders()
         {
             Headers = new List<HttpHeader>();
+        }
+        
+        public HttpHeader this[int index]  
+        {  
+            get { return Headers[index]; }  
+            set { Headers.Insert(index, value); }  
+        } 
+
+        public IEnumerator<HttpHeader> GetEnumerator()
+        {
+            return Headers.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
         }
     }
 }
