deb5db6669a050125ef138f1b0aca51af236a840
Kameko Kirishiki <kameko.k@outlook.com>
2020-04-06 00:57:02 -0400

    remove uneeded usings

--- Caesura.LibNetwork/HttpStatusCode.cs
+++ Caesura.LibNetwork/HttpStatusCode.cs
@@ -1,11 +1,6 @@
 
 namespace Caesura.LibNetwork
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Threading.Tasks;
-    
     public enum HttpStatusCode
     {
         //
