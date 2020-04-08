cbb23877b62a1f017dc38b365f6581131a2a56fd
Kameko Kirishiki <kameko.k@outlook.com>
2020-04-08 13:35:19 -0400

    cleanup

--- Caesura.LibNetwork/ITcpSessionFactory.cs
+++ Caesura.LibNetwork/ITcpSessionFactory.cs
@@ -1,10 +1,6 @@
 
 namespace Caesura.LibNetwork
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Threading.Tasks;
-    
     public interface ITcpSessionFactory
     {
         ITcpSession AcceptTcpConnection();
--- Caesura.LibNetwork/TcpSessionFactory.cs
+++ Caesura.LibNetwork/TcpSessionFactory.cs
@@ -2,10 +2,6 @@
 namespace Caesura.LibNetwork
 {
     using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Threading.Tasks;
-    using System.Net;
     using System.Net.Sockets;
     
     public class TcpSessionFactory : ITcpSessionFactory
